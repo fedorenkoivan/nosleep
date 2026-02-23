96602eaea67388612b1d7e59be7f88948d620935
fedorenkoivan <ivan.r.fedorenko@gmail.com>
2026-02-23 13:07:51 +0200

    feat: docker

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000', // Redirects all /api calls to Express
+      '/api': 'http://localhost:3000',
     },
   },
 })
