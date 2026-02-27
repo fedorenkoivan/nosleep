864d530d448df35ec2a9969d69ee59947c798483
knasts <kulyk.anastasiia1@lll.kpi.ua>
2026-02-27 19:54:17 +0200

    auth page

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -5,6 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     server: {
+        watch: {
+            usePolling: true,
+        },
         proxy: {
             "/api": "http://localhost:3000",
         },
